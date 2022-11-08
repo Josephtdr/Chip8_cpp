18f3777a48b71365f8c398dcb254980c42897772
jdRanda <randa.jtd@gmail.com>
2022-11-08 14:45:48 +0000

    rom loading

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -3,7 +3,7 @@
 
 #include<cstring> //memset, 
 #include <cstdint>
-#include <fstream>
+#include <fstream> //streams
 #include <string_view>
 #include <chrono>
 #include <numeric>//accumulate
@@ -30,7 +30,29 @@ Chip8::Chip8()
 
 void Chip8::LoadROM(char const* filename)
 {
+    // Open the file as a stream of binary and move the file pointer to the end
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
 
+    if (file.is_open())
+	{
+		// Get size of file and allocate a buffer to hold the contents
+		std::streampos size = file.tellg();
+		char* buffer = new char[size];
+
+		// Go back to the beginning of the file and fill the buffer
+		file.seekg(0, std::ios::beg);
+		file.read(buffer, size);
+		file.close();
+
+		// Load the ROM contents into the Chip8's memory, starting at 0x200
+		for (long i = 0; i < size; ++i)
+		{
+			memory[START_ADDRESS + i] = buffer[i];
+		}
+
+		// Free the buffer
+		delete[] buffer;
+	}
 }
 
 void Chip8::Cycle()
